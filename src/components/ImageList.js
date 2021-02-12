f7de0eb0abe6f98e35815fdb5ad270f04adbb3fc
kifayatkifoo <kifoo136@gmail.com>
2021-02-12 11:57:35 +0500

    update

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ImagesList = (props)=>{
   const images = props.images.map((image)=>{
-        return <img alt=" " style={{width:'50%'}} key={image.id} src={image.urls.regular}/>
+        return <img alt=" " style={{width:'30%',height:'300px'}} key={image.id} src={image.urls.regular}/>
     });   
     return <div>{images}</div>;
 }
